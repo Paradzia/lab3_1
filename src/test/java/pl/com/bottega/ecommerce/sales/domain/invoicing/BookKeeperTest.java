66d0bb067d009d8f2d08038630a6b413a0471c7e
Parada <paradowskim94@gmail.com>
2018-04-08 18:32:50 +0200

    added test checking if calculateTax method is called twice

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -2,6 +2,7 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
@@ -13,19 +14,18 @@ import java.util.Date;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BookKeeperTest {
 
     private BookKeeper bookKeeper;
 
+    private TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
+    private Id id = new Id("123");
 
 
     @Test public void invoiceWithOnePosition() throws Exception {
 
-        TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
-        Id id = new Id("123");
         ClientData client = new ClientData(id, "test");
         InvoiceRequest invoiceRequest = new InvoiceRequest(client);
 
@@ -42,6 +42,27 @@ public class BookKeeperTest {
         Invoice invoiceResult = bookKeeper.issuance(invoiceRequest,mockedTaxPolicy);
 
         assertThat(invoiceResult.getItems().size(),is(1));
+    }
+
+    @Test public void invoiceWithTwoPositionsShouldCallCalculateTaxMethodTwice() throws Exception{
+
+        ClientData client = new ClientData(id, "test");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(client);
+
+        InvoiceFactory mockedInvoiceFactory = mock(InvoiceFactory.class);
+        bookKeeper = new BookKeeper(mockedInvoiceFactory);
+
+        ProductData product = new ProductData(id,Money.ZERO,"klawiatura",ProductType.STANDARD,new Date());
+        RequestItem item = new RequestItem(product,1,Money.ZERO);
+        invoiceRequest.add(item);
+        invoiceRequest.add(item);
+
+        when(mockedInvoiceFactory.create(client)).thenReturn(new Invoice(id,client));
+        when(mockedTaxPolicy.calculateTax(ProductType.STANDARD,Money.ZERO)).thenReturn(new Tax(Money.ZERO,"tax"));
+
+        Invoice invoiceResult = bookKeeper.issuance(invoiceRequest,mockedTaxPolicy);
+
+        verify(mockedTaxPolicy,times(2)).calculateTax(ProductType.STANDARD,Money.ZERO);
 
     }
 }
